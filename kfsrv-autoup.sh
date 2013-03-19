7a3420dd573ba4338e0b3384d28aa8f9cd48d423
SnZ <snz@spinacz.org>
2013-03-19 16:46:56 +0100

    Fixed

--- kfsrv-autoup.sh
+++ kfsrv-autoup.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 NoPlayers(){
-#/etc/init.d/kfsrv stop
-#/etc/init.d/kfsrv update
-#/etc/init.d/kfsrv start
-echo xxx
+/etc/init.d/kfsrv stop
+/etc/init.d/kfsrv update
+/etc/init.d/kfsrv start
 }
 
 Check(){
 if echo -ne "\\players\\" | nc -u -w 1 127.0.0.1 7717 | grep -Eoq "player_[0-9]+"; then
   sleep 360
-  Recheck
+  Check
 else
   NoPlayers
 fi
